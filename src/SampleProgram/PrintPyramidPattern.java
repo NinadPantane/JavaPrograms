4f269fe235632d21fe8da8bc9749bf1501bbc384
Dell <Dell@DESKTOP-6QB5I7N.Home>
2025-08-04 15:39:41 +0100

    Program to Print Pyramid Pattern

---
+++ src/SampleProgram/PrintPyramidPattern.java
@@ -0,0 +1,41 @@
+package SampleProgram;
+
+import java.util.concurrent.StructuredTaskScope.ShutdownOnSuccess;
+
+public class PrintPyramidPattern {
+
+	/*
+	 *
+	 * Write a Program to Print Pyramid Pattern in Java as shown below 
+		
+		*
+	   ***
+	  *****
+   	 *******
+	
+	 *
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int rows = 4; // You can change this to print more rows
+
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars: 2*i - 1 stars in each row
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+		
+		
+	}
+
+}
